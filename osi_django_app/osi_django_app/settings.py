5b17d8f9959eab486a65c5054a36bb610d1d7568
sujay <sujay@localhost.localdomain>
2019-05-10 23:21:52 +0530

    The newly created app OSI will be used instead of lists app

--- osi_django_app/osi_django_app/settings.py
+++ osi_django_app/osi_django_app/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'OSI.apps.OsiConfig',
     'lists.apps.ListsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
--- osi_django_app/osi_django_app/urls.py
+++ osi_django_app/osi_django_app/urls.py
@@ -18,5 +18,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('lists.urls'))
+    path('', include('OSI.urls'))
 ]
