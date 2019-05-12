7e3b43764e61fb1726b5ed2db40bc6ee61d3245d
sujay <sujay@localhost.localdomain>
2019-05-12 21:36:36 +0530

    added crispy form tags

--- osi_django_app/osi_django_app/settings.py
+++ osi_django_app/osi_django_app/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'crispy_forms'
 ]
 
 MIDDLEWARE = [
@@ -119,3 +120,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
